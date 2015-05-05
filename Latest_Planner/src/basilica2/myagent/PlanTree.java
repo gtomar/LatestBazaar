9b31543c2add0e742bd569a55f838a17656dcfd8
Gaurav Singh Tomar <gtomar@hyderabad.lti.cs.cmu.edu>
2015-05-04 23:14:27 -0400

    latest template

--- Latest_Planner/src/basilica2/myagent/PlanTree.java
+++ Latest_Planner/src/basilica2/myagent/PlanTree.java
@@ -10,12 +10,14 @@ public class PlanTree<String> {
     public PlanTree(String rootData) {
         root = new Node<String>();
         root.name = rootData;
+        root.detailed_name = rootData;
         root.identified = false;
         root.children = new ArrayList<Node<String>>();
     }
 
     public static class Node<String> {
         public String name;
+        public String detailed_name;
         public boolean identified;
         public PlanTree parent;
         public ArrayList<Node<String>> children;
